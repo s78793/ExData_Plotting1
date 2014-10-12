27535dcdd89ef9c4dd6eff09f195c9cd796d0da8
s78793 <s78793@yahoo.com>
2014-10-12 13:52:31 -0700

    Plot 3 Code

--- Plot3.R
+++ Plot3.R
@@ -16,7 +16,7 @@ subset_file <- subset(full_file, subset=(Date >= "2007-02-01" & Date <= "2007-02
 plot(subset_file$Time, subset_file$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "l")
 lines(subset_file$Time, subset_file$Sub_metering_2, col = "red")
 lines(subset_file$Time, subset_file$Sub_metering_3, col = "blue")
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, cex =0.5,
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
